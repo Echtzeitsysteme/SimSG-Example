1e1400d7c539f0779dc42da97268af239008a770
Sebastian E <sebastian.ehmes@gmail.com>
2019-01-16 16:19:14 +0100

    added files

---
+++ src/example/BiochemistryExample.java
@@ -0,0 +1,29 @@
+package example;
+
+import org.simsg.core.simulation.Simulation;
+import org.simsg.core.simulation.SimulationConfigurator;
+import org.simsg.core.utils.Runtimer;
+
+
+public class BiochemistryExample {
+	
+	public static void main(String[] args) {
+		SimulationConfigurator simConfigurator = new SimulationConfigurator();
+		simConfigurator.setModelFolder(System.getProperty("user.dir")+"//models");
+		simConfigurator.setModel("Goldbeter_Koshland");
+		simConfigurator.setEMFPersistence();
+		simConfigurator.setViatraAsEngine(false);
+		simConfigurator.setHybridPMC();
+		simConfigurator.setStochasticSimulation();
+		simConfigurator.setComplexTerminationCondition();
+		Simulation viatraSim = simConfigurator.createSimulation();
+		viatraSim.initializeClocked();
+		viatraSim.runClocked();
+		System.out.println(viatraSim.results());
+		viatraSim.displayResults();
+		viatraSim.finish();
+		
+		System.out.println(Runtimer.getInstance());
+	}
+	
+} 
